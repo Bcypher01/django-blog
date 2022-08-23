d51bf5db57d2f38ebfbe49659d3c6ea567109aaa
Bcypher01 <olumidebabayemi1@gmail.com>
2022-08-23 01:30:52 +0100

    update settings.py

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j!qqmtoyd@@x(shzj3*2e*bkz7=r526i__rvwtu3o+2gfwe8e_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # Application definition
 
