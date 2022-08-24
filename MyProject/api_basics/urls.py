d0d4e97f37e4a850fb3d25ac7938d34b8f04e741
Bcypher01 <olumidebabayemi1@gmail.com>
2022-08-24 08:52:00 +0100

    switch details api to class based view

--- MyProject/api_basics/urls.py
+++ MyProject/api_basics/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('article/', views.article_list),
-    path('detail/<int:pk>/', views.article_detail)    
+    # path('article/', views.article_list),
+    path('article/', views.ArticleAPIView.as_view()),
+    path('detail/<int:id>/', views.ArticleDetails.as_view()),
+    
+    # path('detail/<int:pk>/', views.article_detail)    
 ]
