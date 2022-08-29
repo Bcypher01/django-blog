693da61f475a990278feea1fb4a15c5ff048cd74
Bcypher01 <olumidebabayemi1@gmail.com>
2022-08-29 07:52:16 +0100

    add authentication

--- MyProject/api_basics/urls.py
+++ MyProject/api_basics/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # path('article/', views.article_list),
     path('article/', views.ArticleAPIView.as_view()),
     path('detail/<int:id>/', views.ArticleDetails.as_view()),
-    path('generic/article/', views.GenericAPIView.as_view()),
+    path('generic/article/<int:id>/', views.GenericAPIView.as_view()),
     # path('detail/<int:pk>/', views.article_detail)    
 ]
--- MyProject/api_basics/views.py
+++ MyProject/api_basics/views.py
@@ -9,16 +9,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins
 from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 class GenericAPIView(generics.GenericAPIView, 
                      mixins.ListModelMixin, 
                      mixins.CreateModelMixin, 
-                     mixins.UpdateModelMixin, mixins.RetrieveModelMixin):
+                     mixins.UpdateModelMixin, 
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
     serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
-    
+    queryset = Article.objects.all()    
     lookup_field = 'id'
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     
     def get(self, request, id= None):
         
@@ -27,14 +35,20 @@ class GenericAPIView(generics.GenericAPIView,
         else:
             return self.list(request)
     
-    def post(self, request):
-        return self.create(request)
+    # def post(self, request):
+    #     return self.create(request)
     
     def put(self, request, id=None):
        return self.update(request, id) 
+   
+    def delete(self, request, id):
+        return self.destroy(request, id)
+        
+   
+    
 
 class ArticleAPIView(APIView):
-    def get(self, request):
+    def get(self):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
