a421f0a4a6273177e9ed196cb0b2f5104049eed8
Bcypher01 <olumidebabayemi1@gmail.com>
2022-09-01 17:12:22 +0100

    add router viewsets

--- MyProject/api_basics/urls.py
+++ MyProject/api_basics/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from posixpath import basename
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', views.ArticleViewSet, basename='article')
 
 urlpatterns = [
+    path('viewsets/', include(router.urls)),
+    path('viewsets/<int:pk>', include(router.urls)),
     # path('article/', views.article_list),
     path('article/', views.ArticleAPIView.as_view()),
     path('detail/<int:id>/', views.ArticleDetails.as_view()),
