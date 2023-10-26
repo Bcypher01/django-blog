6ca0339827dadf40dd9b8986cc64d61747514c59
Bcypher01 <olumidebabayemi1@gmail.com>
2023-10-27 00:02:55 +0100

    add auth

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, generics, mixins, viewsets
 from rest_framework.decorators import api_view, APIView
 from .models import Post
@@ -7,31 +8,32 @@ from .serializers import PostSerializer
 from django.shortcuts import get_object_or_404
 
 
-class PostViewset(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+# class PostViewset(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+class PostListCeateView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    # A View for creating and listing posts
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
 
-# class PostListCeateView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
-#     # A View for creating and listing posts
-#     serializer_class = PostSerializer
-#     queryset = Post.objects.all()
+    def get(self, request: Request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-#     def get(self, request: Request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-
-#     def post(self, request: Request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
+    def post(self, request: Request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-# class PostRetrieveUpdateDeleteView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-#     serializer_class = PostSerializer
-#     queryset = Post.objects.all()
+class PostRetrieveUpdateDeleteView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
-#     def get(self, request: Request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
+    def get(self, request: Request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-#     def put(self, request: Request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
+    def put(self, request: Request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-#     def delete(self, request: Request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
+    def delete(self, request: Request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
